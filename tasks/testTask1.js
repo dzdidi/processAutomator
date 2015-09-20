9e10ef48ae762ce8aed4f34aa12563426382f06c
Denis Zalessky <deniszalessky@gmail.com>
2015-09-20 15:03:03 +0200

    adopted for event based usage with example task

--- tasks/testTask1.js
+++ tasks/testTask1.js
@@ -1,20 +1,28 @@
-// both condition and task functions accepts one parameter which is array
+module.exports = function(address, taskName){
+  var eventMessager = require('../../event-messager')(taskName);
+  var taskObj = {
+    name: taskName,
+    address: address,
+    payload: {}
+  }
 
-module.exports = {
-  name: 'testTask1',
-  schedule: ['Delay in days for first execution', '...', 'Delay in days for n-th execution after n-1 -th'],
-  conditionParams: [
-    {'model':'modelName', 'feild':'feildName', 'value': 'value'},
-    {'model':'modelName', 'feild':'feildName', 'value': 'value'}
-  ],
-  condition: function(a){ // should also include logic to check is task scheduled
-    return true;
-  },
-  taskParams: {
-    {'model':'modelName', 'feild':'feildName', 'value': 'value'},
-    {'model':'modelName', 'feild':'feildName', 'value': 'value'}
-  },
-  task: function(a){ //should include logic to state that task is scheduled
-    console.log(a);
+  var conditionObj = {
+    name: "check".concat(taskName);
+    address: address,
+    payload: {}
+  }
+
+  return{
+    condition: function(obj){
+      if(obj.a === obj.b){
+        eventMessager.emit(taskName);
+      } else{
+        console.log("Sorry, conditions for task %s are false", taskName);
+      }
+    },
+
+    task: function(a){
+      console.log(a);
+    }
   }
 };
